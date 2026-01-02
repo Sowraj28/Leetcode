282724cf0f578609ad6a664b93368b279e7d583e
Sowraj <sowrajsethu2004@gmail.com>
2026-01-02 16:11:07 +0530

    Time: 3 ms (46.06%), Space: 17.4 MB (86.06%) - LeetHub

---
+++ 0859-buddy-strings/0859-buddy-strings.py
@@ -0,0 +1,18 @@
+class Solution:
+    def buddyStrings(self, s: str, goal: str) -> bool:
+        if len(s)!=len(goal):
+            return False
+        if s==goal:
+            return len(set(s))<len(s)
+
+        d=[]
+        for i in range(len(s)):
+            if s[i]!=goal[i]:
+                d.append(i)
+        
+        if len(d)!=2:
+            return False
+        i,j=d
+        
+
+        return s[i]==goal[j] and s[j]==goal[i]
